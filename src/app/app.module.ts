9a68c81557a78f44cdd0286a0df28c76ee2a8200
Arnaud <heyarnaudle@gmail.com>
2020-07-07 22:38:00 +0200

    Using Angular Material for navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoanComponent } from './loan/loan.component';
@@ -9,34 +12,33 @@ import { MonthlyPaymentComponent } from './monthly-payment/monthly-payment.compo
 import { RealEstateSearchComponent } from './real-estate-search/real-estate-search.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-export const appRouteList: Routes = [
-  {
-      path: 'home',
-      component: HomeComponent
-  },
-  {
-      path: 'loan',
-      component: LoanComponent
-  },
-  {
-    path: 'monthly-payment',
-    component: MonthlyPaymentComponent
-  },
-  {
-    path: 'real-estate-search',
-    component: RealEstateSearchComponent
-  },
-  {
-    path: '',
-    redirectTo: '/home',
-    pathMatch: 'full'
-  },
-  {
-      path: '**',
-      redirectTo: 'landing'
-  }
-];
-
+// export const appRouteList: Routes = [
+//   {
+//       path: 'home',
+//       component: HomeComponent
+//   },
+//   {
+//       path: 'loan',
+//       component: LoanComponent
+//   },
+//   {
+//     path: 'monthly-payment',
+//     component: MonthlyPaymentComponent
+//   },
+//   {
+//     path: 'real-estate-search',
+//     component: RealEstateSearchComponent
+//   },
+//   {
+//     path: '',
+//     redirectTo: '/home',
+//     pathMatch: 'full'
+//   },
+//   {
+//       path: '**',
+//       redirectTo: 'landing'
+//   }
+// ];
 
 @NgModule({
   declarations: [
@@ -47,7 +49,9 @@ export const appRouteList: Routes = [
     RealEstateSearchComponent
   ],
   imports: [
-    BrowserModule, RouterModule.forRoot(appRouteList), BrowserAnimationsModule
+    BrowserModule, 
+    // RouterModule.forRoot(appRouteList),
+     BrowserAnimationsModule, MatTabsModule, MatButtonToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
